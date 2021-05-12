3bb27b02e3242d87418dc693ebe4e8de14c37d92
enrique.sampson <enrique.sampson@AFSWWHBUYNVSJY.us.accenturefederal.com>
2021-05-12 15:31:18 -0500

    new model added, home.jsp, previous homeCont=altCont

---
+++ src/main/java/com/organization/mvcproject/MGL_Task1/controller/AltController.java
@@ -0,0 +1,60 @@
+package com.organization.mvcproject.MGL_Task1.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.organization.mvcproject.MGL_Task1.model.Game;
+import com.organization.mvcproject.MGL_Task1.model.Review;
+import com.organization.mvcproject.MGL_Task1.service.GameService;
+
+
+@Controller
+public class AltController {
+
+	@Autowired
+	private GameService gameService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		return "index";
+	}
+
+	@RequestMapping(value = "/review", method = RequestMethod.GET)
+	public ModelAndView review() {
+		return new ModelAndView("review", "command", new Review());
+	}
+
+	@RequestMapping(value = "/addReview", method = RequestMethod.POST)
+	public ModelAndView addReview(Review review, ModelMap model) {
+		if(review.getAuthor().equals("")) {
+			review.setAuthor("anonymous");
+		}
+		return new ModelAndView("result", "submittedReview", review);
+	}
+
+	@RequestMapping(value = "/games", method = RequestMethod.GET)
+	public ModelAndView game() {
+		return new ModelAndView("games", "command", new Game());
+	}
+
+	@RequestMapping(value = "/getAll", method = RequestMethod.GET)
+	public ResponseEntity<List<Game>> fetchAllGames() {
+		return new ResponseEntity<List<Game>>(gameService.retrieveAllGames(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/createGame", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Void> createGame(@RequestBody Game game) {
+		gameService.saveGame(game);
+		return new ResponseEntity<Void>(HttpStatus.CREATED);
+	}
+}
